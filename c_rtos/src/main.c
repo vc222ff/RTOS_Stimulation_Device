8623d6633806f766e4dcda6fba1fd36d950a36c5
Victor Cajfeldt <vc222ff@student.lnu.se>
2025-02-12 00:40:07 +0100

    Functionality testing on Pico

--- c_rtos/src/main.c
+++ c_rtos/src/main.c
@@ -21,6 +21,23 @@ int main()
 {
     stdio_init_all();
 
+    printf("Initializing internal LED");
+    const uint LED_PIN = 15;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    while(true) {
+        printf("Hello, I Am alive in true-loop!");
+        gpio_put(LED_PIN, 1);
+        sleep_ms(500);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(500);
+    }
+
+    while(1) {
+        printf("Hello, I Am alive in 1-loop!");
+    }
+
     xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
     vTaskStartScheduler();
 
